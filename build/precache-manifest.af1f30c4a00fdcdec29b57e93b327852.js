5e113fa2d163f6484deaa06a27cfce225844b8b9
Ricardo Leme <ricardo.leme@gmail.com>
2020-10-16 12:04:19 -0300

    Alterações deploy

---
+++ build/precache-manifest.af1f30c4a00fdcdec29b57e93b327852.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e94c6cc3a2f2fa0f385ec0c1e6e4b3",
+    "url": "/exemplo-react-hook-soma-material-ui/index.html"
+  },
+  {
+    "revision": "a84135d9373ac5a51ec5",
+    "url": "/exemplo-react-hook-soma-material-ui/static/js/2.e1961c39.chunk.js"
+  },
+  {
+    "revision": "84813d2432ab9c27693f",
+    "url": "/exemplo-react-hook-soma-material-ui/static/js/main.54d01f4d.chunk.js"
+  },
+  {
+    "revision": "e0d68ee1bdfbecc9d7c8",
+    "url": "/exemplo-react-hook-soma-material-ui/static/js/runtime~main.079dd0c0.js"
+  }
+]);
